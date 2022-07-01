26299479e5232d2521a3c31b2eeb745ea9af7c02
Kristopher Bright <kris@Kris-13-MacBook-Pro.local>
2022-07-01 17:48:01 -0400

    update for accuracy

--- chat-app-site/tests/unit/routes/individual-message-test.js
+++ chat-app-site/tests/unit/routes/individual-message-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | history', function (hooks) {
+module('Unit | Route | individual-message', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:history');
+    let route = this.owner.lookup('route:individual-message');
     assert.ok(route);
   });
 });
